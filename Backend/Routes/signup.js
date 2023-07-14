fc26a3e603adaa80db1207a7cb99f0b47b9bc234
prathimasree08 <61582703+prathimasree08@users.noreply.github.com>
2023-07-14 16:44:54 +0530

    Delete Backend directory

--- Backend/Routes/signup.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userController = require('../Controllers/user');
-
-router.post('/user/signup', userController.postSignUpUser);
-
-module.exports = router;
